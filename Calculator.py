84818ff5539637ea367d5bbf178e2452cb2b8449
calicer <mittalcharu22@gmail.com>
2024-09-11 15:13:05 +0530

    adding calculator python file

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+"""
+calculator.py
+Calculator library containing basic math operations.
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+def multily(first_term, second_term):
+    return first_term * second_term
